531b3e38b5a20f37e70cb32f84e5614d898a6f0e
Евгений <admin@yemorkovin.ru>
2025-05-09 10:54:02 +0300

    add frontend

--- app/apps.py
+++ app/apps.py
@@ -1,5 +1,8 @@
+import os
 from django.apps import AppConfig
 
 class YourAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'app'
+
+
--- navigator/settings.py
+++ navigator/settings.py
@@ -175,7 +175,7 @@ CELERY_TIMEZONE = 'Europe/Moscow'
 CELERY_BEAT_SCHEDULE = {
     'update-weather-every-hour': {
         'task': 'app.tasks.update_schedule',
-        'schedule': 120.0,  # Каждый час (в секундах)
+        'schedule': 1800.0,  # Каждый час (в секундах)
         # Можно использовать также:
         # 'schedule': crontab(minute=0, hour='*/1'),  # Каждый час в 00 минут
     },
